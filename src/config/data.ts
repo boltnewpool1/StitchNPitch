548e838d2c3310b87d6ade404d49b82d558847b7
boltnewpool1 <boltnewpool1@gmail.com>
2025-10-01 23:18:27 +0530

    Updated data.ts

--- src/config/data.ts
+++ src/config/data.ts
@@ -43,230 +43,226 @@ export const PURGE_PASSWORD = "InternationalMessaging@20";
 export const GUIDES: Guide[] = [
   // Abdul Rahman - General
   {
-    id: 1,
-    name: "Danny Polepaka",
-    department: "General",
-    supervisor: "Abdul Rahman"
+    "id": 1,
+    "name": "Danny Polepaka",
+    "department": "General",
+    "supervisor": "Abdul Rahman"
   },
   {
-    id: 2,
-    name: "Kusuma Manasa",
-    department: "General",
-    supervisor: "Abdul Rahman"
+    "id": 2,
+    "name": "Uppunuthula Harikumar",
+    "department": "General",
+    "supervisor": "Abdul Rahman"
   },
   {
-    id: 3,
-    name: "Kondamedi Nithin",
-    department: "General",
-    supervisor: "Abdul Rahman"
+    "id": 3,
+    "name": "Shivani Akkapelli",
+    "department": "General",
+    "supervisor": "Abdul Rahman"
   },
   {
-    id: 4,
-    name: "Likhitha Nalke",
-    department: "General",
-    supervisor: "Abdul Rahman"
+    "id": 4,
+    "name": "Prashanth Arukala",
+    "department": "General",
+    "supervisor": "Prashanth G C"
   },
   {
-    id: 5,
-    name: "Bharath Nalla",
-    department: "General",
-    supervisor: "Abdul Rahman"
+    "id": 5,
+    "name": "Swathi, Botha",
+    "department": "General",
+    "supervisor": "Prashanth G C"
   },
   {
-    id: 6,
-    name: "Pavan Kalyan Bolla",
-    department: "General",
-    supervisor: "Abdul Rahman"
+    "id": 6,
+    "name": "Gudipati Sreelatha",
+    "department": "General",
+    "supervisor": "Prashanth G C"
   },
   {
-    id: 7,
-    name: "Uppunuthula Harikumar ",
-    department: "General",
-    supervisor: "Abdul Rahman"
+    "id": 7,
+    "name": "Shravya Nerella",
+    "department": "General",
+    "supervisor": "Abdul Rahman"
   },
   {
-    id: 8,
-    name: "Shravya Nerella",
-    department: "General",
-    supervisor: "Abdul Rahman"
-  },
-   {
-    id: 9,
-    name: "Sanjana Sry Akkati",
-    department: "General",
-    supervisor: "Abdul Rahman"
+    "id": 8,
+    "name": "Veeramreddy venkata sai kumar reddy",
+    "department": "General",
+    "supervisor": "Prashanth G C"
   },
   {
-    id: 10,
-    name: "Shivani Akkapelli",
-    department: "General",
-    supervisor: "Abdul Rahman"
+    "id": 9,
+    "name": "Likhitha Nalke",
+    "department": "General",
+    "supervisor": "Abdul Rahman"
   },
   {
-    id: 11,
-    name: "ETYALA PRASAD ",
-    department: "General",
-    supervisor: "Abdul Rahman"
+    "id": 10,
+    "name": "Mounika, Mittapelli",
+    "department": "General",
+    "supervisor": "Prashanth G C"
   },
   {
-    id: 12,
-    name: "Vagdevi Dasari",
-    department: "General",
-    supervisor: "Abdul Rahman"
-  },
-   {
-    id: 13,
-    name: "Raghunayakula Sushmitha",
-    department: "General",
-    supervisor: "Abdul Rahman"
-  },  
-
-  // Abdul Rahman Hosting
-  {
-    id: 14,
-    name: "Manisha Thakur",
-    department: "Hosting",
-    supervisor: "Abdul Rahman"
+    "id": 11,
+    "name": "Bharath Nalla",
+    "department": "General",
+    "supervisor": "Abdul Rahman"
   },
   {
-    id: 15,
-    name: "Nikhil Ramagiri",
-    department: "Hosting",
-    supervisor: "Abdul Rahman"
+    "id": 12,
+    "name": "Kondamedi Nithin",
+    "department": "General",
+    "supervisor": "Abdul Rahman"
   },
   {
-    id: 16,
-    name: "Alli Uday Kiran",
-    department: "Hosting",
-    supervisor: "Abdul Rahman"
+    "id": 13,
+    "name": "Gopina  Nikhil",
+    "department": "General",
+    "supervisor": "Prashanth G C"
   },
   {
-    id: 17,
-    name: "Rajkumar Dhude",
-    department: "Hosting",
-    supervisor: "Abdul Rahman"
+    "id": 14,
+    "name": "Kotagiri  Sai Raj",
+    "department": "General",
+    "supervisor": "Prashanth G C"
   },
   {
-    id: 18,
-    name: "Swetha Santhpale",
-    department: "Hosting",
-    supervisor: "Abdul Rahman"
+    "id": 15,
+    "name": "Raghunayakula Sushmitha",
+    "department": "General",
+    "supervisor": "Abdul Rahman"
   },
   {
-    id: 19,
-    name: "Shalini Lingala",
-    department: "Hosting",
-    supervisor: "Abdul Rahman"
+    "id": 16,
+    "name": "Kusuma Manasa",
+    "department": "General",
+    "supervisor": "Abdul Rahman"
   },
   {
-    id: 20,
-    name: "Garidepalli Thriveni",
-    department: "Hosting",
-    supervisor: "Abdul Rahman"
+    "id": 17,
+    "name": "Vemula Bhanuja",
+    "department": "General",
+    "supervisor": "Prashanth G C"
   },
   {
-    id: 21,
-    name: "Nallamala Srujan",
-    department: "Hosting",
-    supervisor: "Abdul Rahman"
-  },
-    {
-    id: 22,
-    name: "Shruthi Bairi",
-    department: "Hosting",
-    supervisor: "Abdul Rahman"
+    "id": 18,
+    "name": "Bhargavi Macherla",
+    "department": "General",
+    "supervisor": "Chandrasekhar Palem"
   },
   {
-    id: 23,
-    name: "Murali krishna Bantu",
-    department: "Hosting",
-    supervisor: "Abdul Rahman"
+    "id": 19,
+    "name": "Satla Vinay",
+    "department": "General",
+    "supervisor": "Chandrasekhar Palem"
   },
   {
-    id: 24,
-    name: "Mounika  Gadeela",
-    department: "Hosting",
-    supervisor: "Abdul Rahman"
+    "id": 20,
+    "name": "Vennela Karjugutha",
+    "department": "General",
+    "supervisor": "Chandrasekhar Palem"
   },
   {
-    id: 25,
-    name: "Veldi Vijay Kumar",
-    department: "Hosting",
-    supervisor: "Abdul Rahman"
+    "id": 21,
+    "name": "Kolariya Sharma",
+    "department": "General",
+    "supervisor": "Chandrasekhar Palem"
   },
   {
-    id: 26,
-    name: "Samreen Begum",
-    department: "Hosting",
-    supervisor: "Abdul Rahman"
-  },
-
-  // Palem - General
-  {
-    id: 27,
-    name: "Kolariya Sharma",
-    department: "General",
-    supervisor: "Chandrasekhar Palem"
+    "id": 22,
+    "name": "SAGANTI NIKHITHA",
+    "department": "General",
+    "supervisor": "Chandrasekhar Palem"
   },
   {
-    id: 28,
-    name: "Manasa Patra",
-    department: "General",
-    supervisor: "Chandrasekhar Palem"
+    "id": 23,
+    "name": "Varsha sree Narra",
+    "department": "General",
+    "supervisor": "Chandrasekhar Palem"
   },
   {
-    id: 29,
-    name: "Mitra, Somnath",
-    department: "General",
-    supervisor: "Chandrasekhar Palem"
+    "id": 24,
+    "name": "Reena Aerrabelly",
+    "department": "General",
+    "supervisor": "Chandrasekhar Palem"
   },
   {
-    id: 30,
-    name: "Satla Vinay",
-    department: "General",
-    supervisor: "Chandrasekhar Palem"
+    "id": 25,
+    "name": "Mohith Chaturvedula ",
+    "department": "General",
+    "supervisor": "Chandrasekhar Palem"
   },
   {
-    id: 31,
-    name: "Chandrashekhar Nanneboina",
-    department: "General",
-    supervisor: "Chandrasekhar Palem"
+    "id": 26,
+    "name": "Loukya Deshaboina",
+    "department": "General",
+    "supervisor": "Chandrasekhar Palem"
   },
   {
-    id: 32,
-    name: "SAGANTI NIKHITHA",
-    department: "General",
-    supervisor: "Chandrasekhar Palem"
+    "id": 27,
+    "name": "Soumya Muppidi",
+    "department": "General",
+    "supervisor": "Chandrasekhar Palem"
   },
   {
-    id: 33,
-    name: "Ameena Tabassum",
-    department: "General",
-    supervisor: "Chandrasekhar Palem"
+    "id": 28,
+    "name": "Manasa Patra",
+    "department": "General",
+    "supervisor": "Chandrasekhar Palem"
   },
   {
-    id: 34,
-    name: "Varsha sree Narra",
-    department: "General",
-    supervisor: "Chandrasekhar Palem"
+    "id": 29,
+    "name": "Mitra, Somnath",
+    "department": "General",
+    "supervisor": "Chandrasekhar Palem"
   },
   {
-    id: 35,
-    name: "Soumya Muppidi",
-    department: "General",
-    supervisor: "Chandrasekhar Palem"
+    "id": 30,
+    "name": "Pawan Kumar, Perapaka Jyothier Venkata",
+    "department": "General",
+    "supervisor": "Chandrasekhar Palem"
   },
   {
-    id: 36,
-    name: "Jella Tharun",
-    department: "General",
-    supervisor: "Chandrasekhar Palem"
+    "id": 31,
+    "name": "Chandrashekhar Nanneboina",
+    "department": "General",
+    "supervisor": "Chandrasekhar Palem"
   },
   {
-    id: 37,
-    name: "Pasha Mohd Moulana",
-    department: "General",
-    supervisor: "Chandrasekhar Palem"
+    "id": 32,
+    "name": "Jella Tharun",
+    "department": "General",
+    "supervisor": "Chandrasekhar Palem"
+  },
+  {
+    "id": 33,
+    "name": "Veenanjali Kodam",
+    "department": "General",
+    "supervisor": "Chandrasekhar Palem"
+  },
+  {
+    "id": 34,
+    "name": "Jyothika Mudupathula",
+    "department": "General",
+    "supervisor": "Chandrasekhar Palem"
+  },
+  {
+    "id": 35,
+    "name": "Pasha Mohd Moulana",
+    "department": "General",
+    "supervisor": "Chandrasekhar Palem"
+  },
+  {
+    "id": 36,
+    "name": "Ameena Tabassum",
+    "department": "General",
+    "supervisor": "Chandrasekhar Palem"
+  },
+  {
+    "id": 37,
+    "name": "Sirikonda  Raghu",
+    "department": "General",
+    "supervisor": "Prashanth G C"
   },
   {
     id: 38,
